345b16f625d744a52f82cc08c449e844fa203366
jannik busse <jannik@busse84>
2025-09-30 12:28:54 +0200

    Prototype of color buffer and model matrix inclusion

--- shader/shader.cpp
+++ shader/shader.cpp
@@ -1,10 +1,11 @@
 const char* vertexShaderSource = R"(
 #version 330 core
 	layout(location = 0) in vec2 aPos;
+	uniform mat4 model;
 
 	void main()
 	{
-		gl_Position = vec4(aPos, 0.0, 1.0);
+		gl_Position = model * vec4(aPos, 0.0, 1.0) ;
 	}
 )";
 
@@ -12,8 +13,9 @@ const char *fragmentShaderSource = R"(
 	// Fragment shader
 #version 330 core
 	out vec4 FragColor;
+	uniform vec3 color;
 	void main() {
-		FragColor = vec4(1.0, 0.0, 0.0, 1.0); // red
+		FragColor = vec4(color.x, color.y, color.z, 1.0); // red
 	}
 )";
 
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,10 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>  // for glm::translate, rotate, scale, perspective
+#include <glm/gtc/type_ptr.hpp>          // for glm::value_ptr
+
 #include <shdrmangr.h>
 #include <modelmgr.h>
 #include <GameObject.h>
@@ -9,23 +13,23 @@
 Modelmgr *mdlmgr;
 
 void framebuffer_size_callback(GLFWwindow* w,int width,int height){
-    glViewport(0,0,width,height);
+	glViewport(0,0,width,height);
 }
 
 int main() {
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Window", nullptr, nullptr);
-    if (!window) { glfwTerminate(); return -1; }
-    glfwMakeContextCurrent(window);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Window", nullptr, nullptr);
+	if (!window) { glfwTerminate(); return -1; }
+	glfwMakeContextCurrent(window);
 
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        std::cerr << "Failed to init GLAD\n";
-        return -1;
-    }
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cerr << "Failed to init GLAD\n";
+		return -1;
+	}
 
 	if(shdrmngr_compile_shaders()) 
 	{
@@ -37,22 +41,34 @@ int main() {
 
 	test_function();
 	GameObject go;
-
 	go.go_test();
 
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glm::mat4 model = glm::mat4(1.0f); // identity matrix
+	model = glm::translate(model, glm::vec3(-1, 1, 1));   // position
+																			// model = glm::rotate(model, angle, glm::vec3(0.0f, 1.0f, 0.0f)); // rotation
+																			// model = glm::scale(model, glm::vec3(sx, sy, sz));    // scale
+	glm::vec3 color(1.0f, 2.0f, 0.0f);
+
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 	glUseProgram(defaultShaderProgram);
-    while (!glfwWindowShouldClose(window)) {
-        glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+
+	GLuint modelLoc = glGetUniformLocation(defaultShaderProgram, "model");
+	GLuint colorLoc = glGetUniformLocation(defaultShaderProgram, "color");
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+	glUniform3fv(colorLoc, 1, glm::value_ptr(color));
+
+	while (!glfwWindowShouldClose(window)) {
+		glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
 
 		glBindVertexArray(mdlmgr->square_mesh._VAO);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-    glfwTerminate();
-    return 0;
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwTerminate();
+	return 0;
 }
 
